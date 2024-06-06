df6eb41ed7efeace8db00e7bf34e214e45295086
Pedro Soto <u202020808@upc.edu.pe>
2024-06-05 23:54:19 -0500

    Corregido error en la busqueda de clientes

--- src/main/java/com/eventos/Fiadito/repositories/ClienteRepository.java
+++ src/main/java/com/eventos/Fiadito/repositories/ClienteRepository.java
@@ -4,5 +4,5 @@ import com.eventos.Fiadito.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-    Cliente findByUsername(String username);
+    Cliente findByDni(String dni);
 }
--- src/main/java/com/eventos/Fiadito/repositories/CuentaCorrienteRepository.java
+++ src/main/java/com/eventos/Fiadito/repositories/CuentaCorrienteRepository.java
@@ -4,5 +4,5 @@ import com.eventos.Fiadito.models.CuentaCorriente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CuentaCorrienteRepository extends JpaRepository<CuentaCorriente, Long> {
-    CuentaCorriente findByCliente(Long cliente);
+    CuentaCorriente findByClienteId(Long cliente);
 }
