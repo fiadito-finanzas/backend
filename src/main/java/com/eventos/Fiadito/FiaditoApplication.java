90e09327bf92e51ca5f827ef5b5b02aab8290710
Pedro Soto <u202020808@upc.edu.pe>
2024-06-09 11:26:50 -0500

    Agregando características a modelos

--- src/main/java/com/eventos/Fiadito/FiaditoApplication.java
+++ src/main/java/com/eventos/Fiadito/FiaditoApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 @EntityScan("com.eventos.Fiadito.models")
 @EnableAutoConfiguration
 public class FiaditoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FiaditoApplication.class, args);
 	}
